74b722523e5569f81111a7c20e3601ea6116585e
RenanWindowsMesa <engrtorres@outlook.com>
2023-09-30 10:58:37 -0300

    Database: DB and Models and Initial Migration Created

--- MVCCitel/MVCCitel/Program.cs
+++ MVCCitel/MVCCitel/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MVCCitel.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services
+  .AddEntityFrameworkNpgsql()
+  .AddDbContext<DataContextEF>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
@@ -23,5 +30,18 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+/*try
+{
+    var context = services.GetRequiredService<DataContextEF>();
+    context.Database.Migrate();
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex,"An error occured during migration.");
+}*/
 
 app.Run();
