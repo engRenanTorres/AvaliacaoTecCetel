7875e1faada8bad7db18d456825de6c2887678b0
RenanWindowsMesa <engrtorres@outlook.com>
2023-09-29 22:47:03 -0300

    first commit

---
+++ MVCCitel/MVCCitel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCCitel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
